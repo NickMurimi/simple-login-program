a11f16ef9ed8e5dc51db3178b831af309bd6b5f0
Nick  Murimi <126444800+NickMurimi@users.noreply.github.com>
2023-06-12 22:25:32 +0300

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,16 @@
+print("SECURE LOGIN")
+username = input("username > ")
+password = input("Password > ")
+
+if username == "Ann" and password == "mukami01":
+  print("Hey charmy!")
+elif username == "Nick" and password == "1234":
+  print("Wellcome Master!")
+elif username == "Aaron" and password == "abcd":
+  print("Yow Bro")
+elif username == "Milly" and password == "milly01":
+  print("hey siz!")
+elif username == "Beatrice" and password == "B1234":
+  print("Hellow mum!")
+else:
+  print("wrong username and password!")
